59314979b37d20d21007d740a71962fc06165dc9
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-02 05:45:04 -0600

    selected category of content now displaying

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import local from '../../data/local';
+import health from '../../data/health';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = ({selectCategory}) => {
+    return (
+        <nav>
+            <h1>What's New?</h1>
+            <h3 onClick={event => selectCategory(technology)} >Tech</h3>
+            <h3 onClick={event => selectCategory(local)}>Local</h3>
+            <h3 onClick={event => selectCategory(entertainment)}>Entertainment</h3>
+            <h3 onClick={event => selectCategory(science)}>Science</h3>
+            <h3 onClick={event => selectCategory(health)}>Health</h3>
+        </nav>
+    )
+}
 
 export default Menu;
